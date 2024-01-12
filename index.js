6473058ceb61cdf6baa3519b1548f5f59fce699d
boiwantlearncode <ibetank@gmail.com>
2024-01-12 20:16:10 +0800

    Dashboard completed, status report partialy

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var HTMLheatindex = document.querySelector("#heatindex")
 var HTMLhumidity = document.querySelector("#humidity")
 var HTMLtemperature = document.querySelector("#temperature")
 
+// Decide on the values
+const thresholds = {
+  "heatindex": 30.0,
+  "humidity": 75.0,
+  "temperature": 50.0
+}
+
 async function fetchAsync (url) {
   let response = await fetch(url);
   let data = await response.json();
@@ -13,11 +20,45 @@ setInterval(() => {
     console.log(data)
     var { heatindex, humidity, temp } = data;
     console.log(heatindex, humidity, temp)
-    
+
     HTMLheatindex.innerHTML = heatindex
     HTMLhumidity.innerHTML = humidity
     HTMLtemperature.innerHTML = temp
+
+    checkThresholds();
   })
 }, 500);
 
+
+function checkThresholds() {
+  if (heatindex >= thresholds["heatindex"]) {
+    if (!HTMLheatindex.classList.contains("error")) {
+      HTMLheatindex.classList.add("error")
+    }
+  } else {
+    if (HTMLheatindex.classList.contains("error")) {
+      HTMLheatindex.classList.remove("error")
+    }
+  }
+  
+  if (humidity <= thresholds["humidity"]) {
+    if (!HTMLhumidity.classList.contains("error")) {
+      HTMLhumidity.classList.add("error")
+    }
+  } else {
+    if (HTMLhumidity.classList.contains("error")) {
+      HTMLhumidity.classList.remove("error")
+    }
+  }
+  
+  if (temperature >= thresholds["temperature"]) {
+    if (!HTMLtemperature.classList.contains("error")) {
+      HTMLtemperature.classList.add("error")
+    }
+  } else {
+    if (HTMLtemperature.classList.contains("error")) {
+      HTMLtemperature.classList.remove("error")
+    }
+  }
+}
 
