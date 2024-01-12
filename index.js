6e7230070758ad7b58831ddbb8a604e7699110e0
boiwantlearncode <ibetank@gmail.com>
2024-01-13 07:30:18 +0800

    Final

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Change accordingly
+const URL = "http://192.168.109.81:80" + "/data";
+
+
 var HTMLheatindex = document.querySelector("#heatindex")
 var HTMLhumidity = document.querySelector("#humidity")
 var HTMLtemperature = document.querySelector("#temperature")
@@ -6,7 +10,7 @@ var HTMLtemperature = document.querySelector("#temperature")
 const thresholds = {
   "heatindex": 30.0,
   "humidity": 75.0,
-  "temperature": 50.0
+  "temperature": 50.0,
 }
 
 async function fetchAsync (url) {
@@ -16,14 +20,17 @@ async function fetchAsync (url) {
 }
 
 setInterval(() => {
-  fetchAsync("http://192.168.109.81:80/data").then((data) => {
-    console.log(data)
-    var { heatindex, humidity, temp } = data;
-    console.log(heatindex, humidity, temp)
+  fetchAsync(URL).then((data) => {
+    var { heatindex, humidity, temp, JSON } = data;
+    var { analog } = JSON;
 
     HTMLheatindex.innerHTML = heatindex
     HTMLhumidity.innerHTML = humidity
     HTMLtemperature.innerHTML = temp
+
+    // Here's where the magic happens
+    removeData(chart);
+    addData(chart, analog);
 
     checkThresholds();
   })
@@ -61,4 +68,51 @@ function checkThresholds() {
     }
   }
 }
+
+// Chart.js
+const ctx = document.getElementById('myChart').getContext('2d');
+const labels = Array.from({length: 100}, (_, i) => i+1);
+const options = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  }
+};
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Loudness',
+    data: [],
+    fill: true,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: options
+};
+
+var chart = new Chart(ctx, config);
+
+// function fetchAnalog() {
+//   return Array.from({length: 100}, () => Math.floor(Math.random() * 100));
+// }
+
+function addData(chart, newData) {
+  chart.data.datasets[0].data = newData;
+  chart.update();
+  console.log(chart.data.datasets[0].data);
+}
+  
+function removeData(chart) {
+  chart.data.datasets[0].data = [];
+  chart.update();
+  console.log(chart.data.datasets[0].data);
+}
+
+
 
