962c32901b62331dc77ecf556cfd0ca57a623591
boiwantlearncode <ibetank@gmail.com>
2024-01-13 13:37:31 +0800

    Finals

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ const URL = "http://192.168.109.81:80" + "/data";
 var HTMLheatindex = document.querySelector("#heatindex")
 var HTMLhumidity = document.querySelector("#humidity")
 var HTMLtemperature = document.querySelector("#temperature")
+var HTMLsound = document.querySelector("#sound")
+
+var HTMLtemperaturecard = document.querySelector("#temperature-card")
+var HTMLsoundcard = document.querySelector("#sound-card")
+
+var HTMLtemperaturecapsule = document.querySelector("#temperature-capsule")
+var HTMLsoundcapsule = document.querySelector("#sound-capsule")
 
 // Decide on the values
 const thresholds = {
   "heatindex": 30.0,
   "humidity": 75.0,
-  "temperature": 50.0,
+  "temperature": 60.0,
+  "sound": 1.0,
 }
 
-async function fetchAsync (url) {
+async function fetchAsync(url) {
   let response = await fetch(url);
   let data = await response.json();
   return data;
@@ -21,23 +29,27 @@ async function fetchAsync (url) {
 
 setInterval(() => {
   fetchAsync(URL).then((data) => {
-    var { heatindex, humidity, temp, JSON } = data;
-    var { analog } = JSON;
+    var { heatindex, humidity, temp } = data;
+    var { analog } = data;
+    console.log(analog)
+    var maxAnalog = Math.floor(Math.max(...analog) / 10)
+    console.log(maxAnalog)
 
     HTMLheatindex.innerHTML = heatindex
     HTMLhumidity.innerHTML = humidity
     HTMLtemperature.innerHTML = temp
+    HTMLsound.innerHTML = maxAnalog
 
     // Here's where the magic happens
     removeData(chart);
     addData(chart, analog);
 
-    checkThresholds();
+    checkThresholds(temp, maxAnalog);
   })
 }, 500);
 
 
-function checkThresholds() {
+function checkThresholds(temperatureValue, analogValue) {
   if (heatindex >= thresholds["heatindex"]) {
     if (!HTMLheatindex.classList.contains("error")) {
       HTMLheatindex.classList.add("error")
@@ -47,7 +59,7 @@ function checkThresholds() {
       HTMLheatindex.classList.remove("error")
     }
   }
-  
+
   if (humidity <= thresholds["humidity"]) {
     if (!HTMLhumidity.classList.contains("error")) {
       HTMLhumidity.classList.add("error")
@@ -57,21 +69,41 @@ function checkThresholds() {
       HTMLhumidity.classList.remove("error")
     }
   }
-  
-  if (temperature >= thresholds["temperature"]) {
-    if (!HTMLtemperature.classList.contains("error")) {
-      HTMLtemperature.classList.add("error")
-    }
-  } else {
-    if (HTMLtemperature.classList.contains("error")) {
-      HTMLtemperature.classList.remove("error")
+
+  if (temperatureValue >= thresholds["temperature"]) {
+    if (!HTMLtemperaturecapsule.classList.contains("danger")) {
+      HTMLtemperaturecapsule.innerHTML = "Danger"
+      HTMLtemperaturecapsule.classList.add("danger")
+      HTMLtemperaturecapsule.classList.remove("safe")
+
+      HTMLtemperaturecard.classList.add("danger")
+      HTMLtemperaturecard.getElementsByTagName("h3")[0].style.color = "#5b5b5b"
     }
   }
+
+  if (analogValue >= thresholds["sound"]) {
+    if (!HTMLsoundcapsule.classList.contains("danger")) {
+      HTMLsoundcapsule.innerHTML = "Danger"
+      HTMLsoundcapsule.classList.add("danger")
+      HTMLsoundcapsule.classList.remove("safe")
+
+      HTMLsoundcard.classList.add("danger")
+      HTMLsoundcard.getElementsByTagName("h3")[0].style.color = "#5b5b5b"
+    }
+  }
+  // } else {
+  //   if (HTMLsoundcapsule.classList.contains("danger")) {
+  //     HTMLsoundcapsule.innerHTML = "Safe"
+  //     HTMLsoundcapsule.classList.remove("danger")
+  //     HTMLsoundcapsule.classList.add("safe")
+  //   }
+  // }
 }
 
 // Chart.js
 const ctx = document.getElementById('myChart').getContext('2d');
-const labels = Array.from({length: 100}, (_, i) => i+1);
+const labels = Array.from({ length: 100 }, (_, i) => i + 1);
+//const labels = analog
 const options = {
   scales: {
     y: {
@@ -105,14 +137,11 @@ var chart = new Chart(ctx, config);
 function addData(chart, newData) {
   chart.data.datasets[0].data = newData;
   chart.update();
-  console.log(chart.data.datasets[0].data);
+  // console.log(chart.data.datasets[0].data);
 }
-  
+
 function removeData(chart) {
   chart.data.datasets[0].data = [];
   chart.update();
-  console.log(chart.data.datasets[0].data);
+  // console.log(chart.data.datasets[0].data);
 }
-
-
-
